07c743ac038536b7d963e479741333ccd8f4e77c
taouamwalideddine <wtaouam@gmail.com>
2025-09-26 17:00:46 +0100

    NUTR-233 added router

---
+++ src/routes/reports.routes.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/weekly', (req, res) => {
+});
+
+//  Progress 
+router.get('/progress', (req, res) => {
+});
+
+// alerts
+router.get('/Alerts',(req, res) =>{
+})
+// admin
+router.get('Admin review',(req,res) =>{
+})
+// export
+module.exports = router;

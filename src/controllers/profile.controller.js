558bf454cbc08dac3542506b428b0954cf7c34c3
taouamwalideddine <wtaouam@gmail.com>
2025-09-29 15:54:43 +0100

    feat: NUTR-155 working on the controller

--- src/controllers/profile.controller.js
+++ src/controllers/profile.controller.js
@@ -1,1 +1,13 @@
 const profileService = require('../services/profile.service');
+
+module.exports = {
+  getProfile: async (req, res, next) => {
+    try {
+      const userId = req.user.id; 
+      const profileData = await profileService.fetchUserProfile(userId);      
+      res.json(profileData);
+    } catch (error) {
+      next(error);
+    }
+  }
+};

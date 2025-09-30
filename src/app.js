900353ac1ff3e59522aa3b137b35e3d4f555a782
MrNobodyCode <essadeq.billouche@gmail.com>
2025-09-30 14:54:21 +0100

    refactor: make the server.js the only entry point and make app.js for routing redirection

--- src/app.js
+++ src/app.js
@@ -1,10 +1,31 @@
 import express from 'express';
-import reportRoutes from './routes/reportRoutes.js';
-import profileRoutes from './routes/profile.routes.js';
+import path from 'path';
+import authRouter from './routes/auth.routes.js'
+import mealAnalysisRouter from './routes/mealAnalysis.routes.js'
+import profileRouter from './routes/profile.routes.js'
+import reportsRouter from './routes/profile.routes.js'
+import aiRouter from './routes/ai.routes.js'
+
+
+import { fileURLToPath } from 'url';
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
+app.use(express.json());
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
-app.use('/reports', reportRoutes);
-app.use('/profile', profileRoutes);
+app.get('/', (req, res) => {
+    res.render("home/home");
+});
+
+app.use('/auth', authRouter);
+app.use('/dashboard/meal', mealAnalysisRouter);
+app.use('/dashboard/profile', profileRouter);
+app.use('/dashboard/reports', reportsRouter);
+app.use('/dashboard/ai', aiRouter);
 
 export default app;
--- src/server.js
+++ src/server.js
@@ -1,18 +1,4 @@
-import express from "express";
-import path from "path";
-import { fileURLToPath } from "url";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-const app = express();
-app.use(express.json());
-app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
-
-app.get('/', (req, res) => {
-    res.render("home/home");
-});
+import app from "./app.js"
 
 app.listen(3000, () => {
     console.log("server running on port 3000");

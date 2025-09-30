cb338408138352766b2e061c80662b3e922cdc1f
MrNobodyCode <essadeq.billouche@gmail.com>
2025-09-30 15:49:55 +0100

    FEAT : init routes

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import * as authController from '../controllers/auth.controller.js';
-import * as authMiddleware from '../middlewares/auth.middleware.js';
 
 const router = express.Router();
 
-router.post('/register', authMiddleware.validateRegister, authController.postRegister);
-router.post('/login', authMiddleware.validateLogin, authController.postLogin);
+router.post('/register', authController.postRegister);
+router.get('/register', authController.getRegister);
+router.get('/login', authController.getLogin);
+router.post('/login', authController.postLogin);
 router.post('/logout', authController.logout);
 
 export default router;

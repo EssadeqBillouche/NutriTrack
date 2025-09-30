ed96c794e23fd9957cb9d3afa7aa3637b25638c7
MrNobodyCode <essadeq.billouche@gmail.com>
2025-09-30 15:51:40 +0100

    FIX: fix the server loading problem

--- src/app.js
+++ src/app.js
@@ -5,10 +5,9 @@ import mealAnalysisRouter from './routes/mealAnalysis.routes.js'
 import profileRouter from './routes/profile.routes.js'
 import reportsRouter from './routes/profile.routes.js'
 import aiRouter from './routes/ai.routes.js'
-
+import expressLayouts from 'express-ejs-layouts';
 
 import { fileURLToPath } from 'url';
-
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -16,10 +15,12 @@ const __dirname = path.dirname(__filename);
 const app = express();
 app.use(express.json());
 app.set("view engine", "ejs");
+app.use(expressLayouts);
+app.set('layout', 'auth/main');
 app.set("views", path.join(__dirname, "views"));
 
 app.get('/', (req, res) => {
-    res.render("home/home");
+    res.render("home/home", { title : 'Home'});
 });
 
 app.use('/auth', authRouter);

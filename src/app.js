8d7d77c1278c7b6b485db39032bf0f648b4a9533
essadeq billouche <essadeq.billouche@gmail.com>
2025-10-01 14:38:52 +0100

    add middlware for static files

--- src/app.js
+++ src/app.js
@@ -13,7 +13,10 @@ import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+
 const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.json());
 app.set("view engine", "ejs");
 app.use(expressLayouts);
